abc4eb5bf7833f91128121b0d3d95c4a955d33e2
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2022-04-22 01:44:22 +0200

    Updated dependencies

--- nova-core/src/main/java/de/m4rc3l/nova/core/Validatable.java
+++ nova-core/src/main/java/de/m4rc3l/nova/core/Validatable.java
@@ -5,8 +5,8 @@ import de.m4rc3l.nova.core.exception.ValidationException;
 public interface Validatable {
 
   /**
-   * Validates the contents of properties and also {@link ValidationUtils#notNull(String,
-   * Object)}.
+   * Validates the contents of properties and also
+   * {@link de.m4rc3l.nova.core.utils.ValidationUtils#notNull(String, Object)}.
    *
    * @throws ValidationException if the validation fails
    */
@@ -15,8 +15,8 @@ public interface Validatable {
   }
 
   /**
-   * Validates the contents of properties, but not {@link ValidationUtils#notNull(String,
-   * Object)}.
+   * Validates the contents of properties, but not
+   * {@link de.m4rc3l.nova.core.utils.ValidationUtils#notNull(String, Object)}.
    *
    * @throws ValidationException if the validation fails
    */
